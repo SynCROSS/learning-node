79cebe8a877ac75e8d5cd7191fdd80afd7114d66
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2020-11-08 12:13:26 +0900

    New Twinkle

---
+++ Twinkle/passport/index.js
@@ -0,0 +1,23 @@
+const passport = require('C:/Users/kuuha/AppData/Local/Yarn/Data/global/node_modules/passport');
+const local = require('./localStrategy');
+const kakao = require('./kakaoStrategy');
+const User = require('../models/user');
+
+module.exports = () => {
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  passport.deserializeUser((id, done) => {
+    User.findOne({
+      where: { id },
+      include: [
+        { model: User, attributes: ['id', 'nick'], as: 'Followers' },
+        { model: User, attributes: ['id', 'nick'], as: 'Followings' },
+      ],
+    })
+      .then(user => done(null, user))
+      .catch(error => done(error));
+  });
+  local();
+  kakao();
+};
