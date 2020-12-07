bdf63ad9e49f3ff5e8c8481389559e0228834ade
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2020-12-07 22:49:04 +0900

    Learning WebSocket with Project

---
+++ gif-chat-app/app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
+
+require('dotenv').config();
+
+const webSocket = require('./socket');
+const indexRouter = require('./routes');
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'html');
+
+nunjucks.configure('views', { express: app, watch: true });
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json);
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: { httpOnly: true, secure: false },
+  }),
+);
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} Router Ain't Exist`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.locals.message = error.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? error : {};
+  res.status(error.status || 500);
+  res.render('error');
+});
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log('Server is Running on Port', port);
+});
+
+webSocket(server);
