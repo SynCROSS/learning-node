a0a99e9dfdee802b53e3de9ced304e6241de173f
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2021-01-27 19:21:23 +0900

    readline

--- 14 node-cli/index.js
+++ 14 node-cli/index.js
@@ -1,3 +1,24 @@
 #! /usr/bin/env node
-// * npx cli one two three four to execute this
-console.log('Hello World!', process.argv);
+// * npx cli  to execute this
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('Is Javascript the Best [y/n]: ', answer => {
+  switch (answer) {
+    case 'y':
+      console.log('I recommend you try the typescript.');
+      rl.close();
+      break;
+    case 'n':
+      console.log('Right, JS is not the best');
+      rl.close();
+      break;
+    default:
+      rl.close();
+      break;
+  }
+});
